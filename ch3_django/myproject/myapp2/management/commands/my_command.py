c67943dda0f3e1ff97f6f091ad65929c6a26179b
BigBadMuttuh <bigbadantonio@gmail.com>
2024-05-01 16:42:26 +0300

    2024-05-01_1

---
+++ ch3_django/myproject/myapp2/management/commands/my_command.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'My command'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('My command')
